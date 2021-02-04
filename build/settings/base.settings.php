a506ad88b21c63ef7a9b2439b46bfe228ecddb1f
Jacob Sanford <jsanford@unb.ca>
2021-02-04 09:56:07 -0400

    DOCKW-32 Migrate to GitHub actions

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -5,10 +5,15 @@
  * Include global settings overrides here.
  */
 
- // Specify install profile.
- $settings['install_profile'] = 'minimal';
+// Specify install profile.
+$settings['install_profile'] = 'minimal';
 
 // Redis.
 $settings['cache_prefix']['default'] = 'nble_';
 $conf['chq_redis_cache_enabled'] = TRUE;
 require_once dirname(__FILE__) . "/settings.redis.inc";
+
+// Newrelic.
+if (extension_loaded('newrelic')) {
+  require_once dirname(__FILE__) . "/settings.newrelic.inc";
+}
